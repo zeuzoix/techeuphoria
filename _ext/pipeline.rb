40e3910c16abd354e1289d5158d48a952055a7d4
Conrad Gomes <conrad.s.j.gomes@gmail.com>
2014-06-11 00:32:00 -0400

    After completing steps till http://mojavelinux.github.io/decks/docs-workshop/oscon2013/index.html#86.0
    Signed-off-by: Conrad Gomes <conrad.s.j.gomes@gmail.com>

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,16 @@
+require './stylesheets/_themes/lib/functions.rb'
 require 'zurb-foundation'
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new '/news'
+  helper Awestruct::Extensions::Partial
+  extension Awestruct::Extensions::Posts.new '/posts'
+  extension Awestruct::Extensions::Paginator.new :posts, '/index',
+    :page_name => 'posts/page/', :per_page => 5
+  extension Awestruct::Extensions::Tagger.new :posts, '/index', 'posts/tag', 
+    :per_page => 5
+  extension Awestruct::Extensions::Atomizer.new :posts, 'feed.xml',
+    :num_entries => 10
+
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
   # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
