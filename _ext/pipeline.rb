f8fe4a52b00a7b25aa44073422e7c7a327dfd354
Conrad Gomes <conrad.s.j.gomes@gmail.com>
2014-06-28 05:22:29 -0400

    Adding blog section with full width blog posts
    Signed-off-by: Conrad Gomes <conrad.s.j.gomes@gmail.com>

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,6 +6,8 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new '/posts'
   extension Awestruct::Extensions::Paginator.new :posts, '/index',
     :page_name => 'posts/page/', :per_page => 5
+  extension Awestruct::Extensions::Paginator.new :posts, '/blog',
+    :page_name => 'posts/page/', :per_page => 10
   extension Awestruct::Extensions::Tagger.new :posts, '/index', 'posts/tag', 
     :per_page => 5
   extension Awestruct::Extensions::Atomizer.new :posts, 'feed.xml',
