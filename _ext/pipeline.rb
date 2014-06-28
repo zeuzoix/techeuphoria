741f4d34b7a68d03eeb4e44191ae91524c223dbc
Conrad Gomes <conrad.s.j.gomes@gmail.com>
2014-06-28 07:44:47 -0400

    Adding Tagcloud extension and indexifier
    Signed-off-by: Conrad Gomes <conrad.s.j.gomes@gmail.com>

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,15 +3,13 @@ require 'zurb-foundation'
 
 Awestruct::Extensions::Pipeline.new do
   helper Awestruct::Extensions::Partial
-  extension Awestruct::Extensions::Posts.new '/posts'
-  extension Awestruct::Extensions::Paginator.new :posts, '/index',
-    :page_name => 'posts/page/', :per_page => 5
-  extension Awestruct::Extensions::Paginator.new :posts, '/blog',
-    :page_name => 'posts/page/', :per_page => 10
-  extension Awestruct::Extensions::Tagger.new :posts, '/index', 'posts/tag', 
-    :per_page => 5
-  extension Awestruct::Extensions::Atomizer.new :posts, 'feed.xml',
-    :num_entries => 10
+  extension Awestruct::Extensions::Posts.new('/posts')
+  extension Awestruct::Extensions::Paginator.new(:posts, '/index', :page_name => 'posts/page/', :per_page => 5)
+  extension Awestruct::Extensions::Paginator.new( :posts, '/blog', :page_name => 'posts/page/', :per_page => 10)
+  extension Awestruct::Extensions::Tagger.new( :posts, '/index', 'posts/tag', :per_page => 5)
+  extension Awestruct::Extensions::TagCloud.new( :posts, '/posts/tags/index.html', :layout=>:base, :title=>'Blog Tags' )
+  extension Awestruct::Extensions::Indexifier.new()
+  extension Awestruct::Extensions::Atomizer.new( :posts, 'feed.xml', :num_entries => 10)
 
   # extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
